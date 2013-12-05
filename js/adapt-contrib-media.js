3327c846708e9345f573cea1629699e9b8406172
Chris Steele <chris.steele@kineo.com>
2013-12-05 13:28:52 +0000

    basic task complete, pending #8829

---
+++ js/adapt-contrib-media.js
@@ -0,0 +1,46 @@
+define(function(require) {
+
+	var Popup = require("components/adapt-contrib-media/js/popup");
+	var Adapt = require("coreJS/adapt");
+	var ComponentView = require("coreViews/componentView");
+	var Handlebars = require('handlebars');
+
+    var Media = ComponentView.extend({
+        
+		popUp: Popup.extend({
+			template: Handlebars.templates.transcript
+		}),
+		
+		events: {
+			'inview':'inview'
+		},
+		
+        postRender: function() {
+            console.log("rendering");
+			this.updateRatio();
+            this.setReadyStatus();
+            this.setCompletionStatus();
+			this.$('a.show-transcript').on('click', _.bind(this.showTranscript, this));
+        },
+		
+		inview: function(event, visible) {
+			if (visible) this.model.set('complete',true);
+			this.$el.off('inview');
+		},
+		
+		updateRatio: function() {
+			var width = this.$('.widget').width();
+			this.$('.widget object').width(width).height((width/16)*9);
+			this.$('.widget video').width(width).height((width/16)*9);
+		},
+		
+		showTranscript: function(event) {
+			event.preventDefault(); 
+			new this.popUp({model:this.model});
+		}
+        
+    });
+    
+    Adapt.register("media", Media);
+    
+});
