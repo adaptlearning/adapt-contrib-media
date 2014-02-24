b1818dc4ff5f1d6284b9410a4749d28700a48ee2
Kevin Corry <kevinc@learningpool.com>
2014-02-24 11:30:40 +0000

    Fixes #21 Listen to video/audio dataloaded event and set ready status to ensure hidden elements do not interrupt pageView:ready event

--- js/adapt-contrib-media.js
+++ js/adapt-contrib-media.js
@@ -1,52 +1,57 @@
 /*
 * adapt-contrib-media
 * License - http://github.com/adaptlearning/adapt_framework/LICENSE
-* Maintainers - Chris Steele <chris.steele@kineo.com>, Daryl Hedley <darylhedley@hotmail.com>
+* Maintainers - Chris Steele <chris.steele@kineo.com>, Daryl Hedley <darylhedley@hotmail.com>,
+*               Kevin Corry <kevinc@learningpool.com>
 */
 define(function(require) {
 
-	var mep = require("components/adapt-contrib-media/js/mep");
-	var Adapt = require("coreJS/adapt");
-	var ComponentView = require("coreViews/componentView");
-	var Handlebars = require('handlebars');
+  var mep = require("components/adapt-contrib-media/js/mep");
+  var Adapt = require("coreJS/adapt");
+  var ComponentView = require("coreViews/componentView");
+  var Handlebars = require('handlebars');
 
-    var Media = ComponentView.extend({
+  var Media = ComponentView.extend({
 
-    	events: {
-    		'inview':'inview'
-    	},
+    events: {
+      'inview':'inview'
+    },
 
-		preRender: function() {
-			this.listenTo(Adapt, 'device:resize', this.onScreenSizeChanged);
-		},
+    preRender: function() {
+      this.listenTo(Adapt, 'device:resize', this.onScreenSizeChanged);
+      // Listen for loadeddata event and set ready status - some browsers
+      // do not load media content if the media element is hidden.
+      this.$('audio, video').on('loadeddata', this.setReady());
+    },
 
-		onScreenSizeChanged: function() {
-			this.$('audio, video').width(this.$('.component-widget').width());
-		},
-		
-        postRender: function() {
-			this.mediaElement = this.$('audio, video').mediaelementplayer({
-				pluginPath:'assets/', 
-				success: _.bind(function (mediaElement, domObject) {
-					this.setReadyStatus();
-			        mediaElement.addEventListener('ended', _.bind(function() {
-			            this.setCompletionStatus(); 
-			        }, this), false);
-				}, this),
-				features: ['playpause','progress','current','duration']
-			});
-        },
-
-        inview: function(event, visible) {
-        	if (this.model.get('media')._allowCompletionOnInview) {
-        		if (visible) {
-        			this.setCompletionStatus();
-        		}
-        	}
-        }
-
-    });
+    onScreenSizeChanged: function() {
+      this.$('audio, video').width(this.$('.component-widget').width());
+    },
     
-    Adapt.register("media", Media);
-    
+    postRender: function() {
+      this.mediaElement = this.$('audio, video').mediaelementplayer({
+        pluginPath:'assets/', 
+        success: _.bind(function (mediaElement, domObject) {
+          this.setReady();
+        }, this),
+        features: ['playpause','progress','current','duration']
+      });
+    },
+
+    setReady: function() {
+      if (!this.model.get('_isReady')) {
+        this.setReadyStatus();
+      }
+    },
+
+    inview: function(event, visible) {
+      if (visible) {
+        this.setCompletionStatus();
+      }
+    }
+
+  });
+
+  Adapt.register("media", Media);
+
 });
