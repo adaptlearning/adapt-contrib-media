0be84bd6bc19e88dba14054b447626a510f3b505
Lukasz Grela <lukasz.grela@gmail.com>
2015-10-21 10:22:36 +0100

    Issue #128
    split the features test so the tracks are added not only when features is undefined

--- js/adapt-contrib-media.js
+++ js/adapt-contrib-media.js
@@ -31,12 +31,15 @@ define(function(require) {
             var modelOptions = this.model.get('_playerOptions');
 
             if (modelOptions.pluginPath === undefined) modelOptions.pluginPath = 'assets/';
-            if(modelOptions.features === undefined) {
-                modelOptions.features = ['playpause','progress','current','duration'];
-                if (this.model.get('_useClosedCaptions')) {
-                    modelOptions.features = ['playpause','progress','tracks','current','duration'];
-                }
+            if (modelOptions.features === undefined) {
+                modelOptions.features = ['playpause', 'progress', 'current', 'duration'];
             }
+            //at this point features will not be undefined
+            var ccFeature = "tracks";
+            if (this.model.get("_useClosedCaptions") && modelOptions.features.indexOf(ccFeature) == -1) {
+                modelOptions.features.push(ccFeature);
+            }
+            
             if(modelOptions.clickToPlayPause === undefined) modelOptions.clickToPlayPause = true;
 
             modelOptions.success = _.bind(this.onPlayerReady, this);
