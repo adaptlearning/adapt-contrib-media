4ff21083b298e6dff792561b3c92b62e71d37407
Oliver Foster <oliver.foster@kineo.com>
2018-10-03 13:33:01 +0100

    Fixed formation

--- js/adapt-contrib-media.js
+++ js/adapt-contrib-media.js
@@ -177,7 +177,7 @@ define([
 
         cleanUpPlayer: function() {
             this.$('.media-widget').children('.mejs-offscreen').remove();
-            this.$('[role=application]').removeAttr('role').removeAttr("tabindex");
+            this.$('[role=application]').removeAttr('role tabindex')
             this.$('[aria-controls]').removeAttr('aria-controls');
         },
 
