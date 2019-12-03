db301665ac7d9ced3fc6b64e150bcb256552f201
moloko <mleathes+github@gmail.com>
2019-12-03 18:32:49 +0000

    remove vendor prefixes for Chrome, Firefox and Opera fixes https://github.com/adaptlearning/adapt_framework/issues/2557

--- libraries/mediaelement-fullscreen-hook.js
+++ libraries/mediaelement-fullscreen-hook.js
@@ -7,49 +7,46 @@ define([
     $.extend(mejs.MediaElementPlayer.prototype, {
         /**
          * fixes a bug (adaptlearning/adapt_framework#1478)
-         * where the media player going into/coming out of full-screen mode would trigger inview on 
-         * components below it; we therefore need to switch off inview when entering full screen mode 
+         * where the media player going into/coming out of full-screen mode would trigger inview on
+         * components below it; we therefore need to switch off inview when entering full screen mode
          * and switch it back on again after exiting full screen mode
          */
         detectFullscreenMode: function() {
             var vendorPrefix = this.getVendorPrefix();
-            var fsEventName = "on" + vendorPrefix + "fullscreenchange";
+            var fsEventName = 'on' + vendorPrefix + 'fullscreenchange';
 
             if (document[fsEventName] === null) {
                 document[fsEventName] = function fullScreenEventHandler() {
 
-                    // because Firefox just HAS to be different...
-                    var elementName = (vendorPrefix === "moz" ? "mozFullScreenElement" : vendorPrefix + "FullscreenElement");
+                    var elementName = (vendorPrefix === '' ? 'fullscreenElement' : vendorPrefix + 'FullscreenElement');
 
                     if (document[elementName] !== null) {
-                        $.inview.lock("mediaelement");
-                        Adapt.trigger("media:fullscreen:enter");
+                        $.inview.lock('mediaelement');
+                        Adapt.trigger('media:fullscreen:enter');
                     } else {
-                        $.inview.unlock("mediaelement");
-                        Adapt.trigger("media:fullscreen:exit");
+                        $.inview.unlock('mediaelement');
+                        Adapt.trigger('media:fullscreen:exit');
                     }
                 };
             }
             return mepPrototype.detectFullscreenMode.apply(this, arguments);
-        }, 
+        },
 
         /**
-         * unfortunately the fullscreen events and properties are all still vendor-prefixed, see
-         * https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API#Prefixing
-         * This function works out the correct prefix for the current browser.
+         * because the fullscreen events and properties are still vendor-prefixed in some browsers...
          */
         getVendorPrefix: function() {
             var browser = Adapt.device.browser.toLowerCase();
 
-            if (browser === "internet explorer") {
-                return "ms";
+            if (browser === 'internet explorer') {
+                return 'ms';
             }
 
-            if (browser === "firefox") {
-                return "moz";
+            if (browser === 'microsoft edge' || browser === 'safari') {
+                return 'webkit';
             }
 
-            return "webkit"; // Chrome, Safari, Opera and Edge all use the 'webkit' prefix, so default to that
+            return ''; // Chrome, Opera and Firefox no longer require a vendor prefiix
         }
     });
 });
