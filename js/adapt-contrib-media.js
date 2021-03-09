a88cc55ed970fd5215fe01f25bef73cfd50356e7
Eleanor Heath <eleanor.heath@learningpoool.com>
2021-03-09 13:18:38 +0000

    ADAPT-7912 - review comment

--- js/adapt-contrib-media.js
+++ js/adapt-contrib-media.js
@@ -191,7 +191,8 @@ define([
       const media = this.model.get('_media');
       if (!media) return callback();
 
-      if (media.mp3) {
+      if (media.mp3 || media.ogg) {
+        // https://github.com/adaptlearning/adapt_framework/issues/3055
         modelOptions.alwaysShowControls = true;
       }
 
