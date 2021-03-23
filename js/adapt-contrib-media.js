528546efc06b3ede6be9c02d4f0ae77663c696b5
Tom Greenfield <tom.greenfield@kineo.com>
2021-03-23 10:21:49 +0000

    Backport #204

--- js/adapt-contrib-media.js
+++ js/adapt-contrib-media.js
@@ -105,7 +105,15 @@ define([
 
             var modelOptions = this.model.get('_playerOptions');
 
-            if (modelOptions.pluginPath === undefined) modelOptions.pluginPath = 'assets/';
+            if (modelOptions.pluginPath === undefined) {
+                // on the off-chance anyone still needs to use the Flash-based player...
+                _.extend(modelOptions, {
+                    pluginPath: 'https://cdnjs.cloudflare.com/ajax/libs/mediaelement/2.21.2/',
+                    flashName: 'flashmediaelement-cdn.swf',
+                    flashScriptAccess: 'always'
+                });
+            }
+
             if (modelOptions.features === undefined) {
                 modelOptions.features = ['playpause','progress','current','duration'];
                 if (this.model.get('_useClosedCaptions')) {
