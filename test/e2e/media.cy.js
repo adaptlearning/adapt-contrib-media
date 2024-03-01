ae4af0c5300194c733c51a08c702c7e3f570cce5
Lemmy Adams <lemmy.adams@learningpool.com>
2024-03-01 10:04:11 +0000

    Added comments

--- test/e2e/media.cy.js
+++ test/e2e/media.cy.js
@@ -9,18 +9,19 @@ describe('Media', function () {
       cy.visit(`/#/preview/${mediaComponent._id}`);
       const bodyWithoutHtml = mediaComponent.body.replace(/<[^>]*>/g, '');
       
+      // Test basic media component
       cy.testContainsOrNotExists('.media__title', mediaComponent.displayTitle)
       cy.testContainsOrNotExists('.media__body', bodyWithoutHtml)
       cy.testContainsOrNotExists('.media__instruction', mediaComponent.instruction)
 
-      if(mediaComponent._media.mp4) {
+      if (mediaComponent._media.mp4) {
         cy.get('.mejs-mediaelement video').should('have.attr', 'src', mediaComponent._media.mp4)
       }
-      if(mediaComponent._media.poster) {
+      if (mediaComponent._media.poster) {
         cy.get('.mejs-poster img').should('have.attr', 'src', mediaComponent._media.poster)
       }
 
-      if(mediaComponent._transcript) {
+      if (mediaComponent._transcript) {
         cy.get('.media__transcript-body-inline').should('not.be.visible')
         cy.get('button.media__transcript-btn').should('contain', mediaComponent._transcript.inlineTranscriptButton).click()
         cy.get('.media__transcript-body-inline-inner').should('be.visible').should('contain', mediaComponent._transcript.inlineTranscriptBody)
@@ -28,6 +29,7 @@ describe('Media', function () {
         cy.get('.media__transcript-body-inline').should('not.be.visible')
       }
 
+      // Allow the component to load and run external custom tests
       cy.wait(1000)
     });
   });
