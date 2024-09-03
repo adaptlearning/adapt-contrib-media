01b9227877f7ec2d5f627649738b3f754069330c
Brad Simpson <brad.simpson@kineo.com>
2024-09-03 15:40:21 -0600

    Force the default language so that the aria-label can be localised from Adapt

--- js/mediaLibrariesOverrides.js
+++ js/mediaLibrariesOverrides.js
@@ -35,40 +35,40 @@ if (features.hasMozNativeFullScreen) {
   };
 }
 
-// /**
-//  * Force the default language so that the aria-label can be localised from Adapt
-//  * Note: Do not change these, their names and values are required for mapping in mejs
-//  */
-// window.mejs.i18n.locale.language = 'en-US';
-// window.mejs.i18n.locale.strings['en-US'] = {};
-// const ariaLabelMappings = {
-//   playText: 'Play',
-//   pauseText: 'Pause',
-//   stopText: 'Stop',
-//   audioPlayerText: 'Audio Player',
-//   videoPlayerText: 'Video Player',
-//   tracksText: 'Captions/Subtitles',
-//   timeSliderText: 'Time Slider',
-//   muteText: 'Mute Toggle',
-//   unmuteStatusText: 'Unmute',
-//   muteStatusText: 'Mute',
-//   volumeSliderText: 'Volume Slider',
-//   fullscreenText: 'Fullscreen',
-//   goFullscreenText: 'Go Fullscreen',
-//   turnOffFullscreenText: 'Turn off Fullscreen',
-//   noneText: 'None',
-//   skipBackText: 'Skip back %1 seconds',
-//   allyVolumeControlText: 'Use Up/Down Arrow keys to increase or decrease volume.',
-//   progessHelpText: 'Use Left/Right Arrow keys to advance one second, Up/Down arrows to advance ten seconds.'
-// };
+/**
+ * Force the default language so that the aria-label can be localised from Adapt
+ * Note: Do not change these, their names and values are required for mapping in mejs
+ */
+window.mejs.i18n.lang = 'en-US';
+window.mejs.i18n['en-US'] = {};
+const ariaLabelMappings = {
+  playText: 'Play',
+  pauseText: 'Pause',
+  stopText: 'Stop',
+  audioPlayerText: 'Audio Player',
+  videoPlayerText: 'Video Player',
+  tracksText: 'Captions/Subtitles',
+  timeSliderText: 'Time Slider',
+  muteText: 'Mute Toggle',
+  unmuteStatusText: 'Unmute',
+  muteStatusText: 'Mute',
+  volumeSliderText: 'Volume Slider',
+  fullscreenText: 'Fullscreen',
+  goFullscreenText: 'Go Fullscreen',
+  turnOffFullscreenText: 'Turn off Fullscreen',
+  noneText: 'None',
+  skipBackText: 'Skip back %1 seconds',
+  allyVolumeControlText: 'Use Up/Down Arrow keys to increase or decrease volume.',
+  progessHelpText: 'Use Left/Right Arrow keys to advance one second, Up/Down arrows to advance ten seconds.'
+};
 
-// Adapt.on('app:dataReady', () => {
-//   // Populate the aria labels from the _global._components._media
-//   const dynamicLabels = window.mejs.i18n.locale.strings['en-US'];
-//   const fixedDefaults = window.mejs.MepDefaults;
-//   const globals = Adapt.course.get('_globals')?._components?._media;
-//   for (const k in ariaLabelMappings) {
-//     dynamicLabels[ariaLabelMappings[k]] = globals[k] ?? ariaLabelMappings[k];
-//     fixedDefaults[k] = dynamicLabels[ariaLabelMappings[k]];
-//   }
-// });
+Adapt.on('app:dataReady', () => {
+  // Populate the aria labels from the _global._components._media
+  const dynamicLabels = window.mejs.i18n['en-US'];
+  const fixedDefaults = window.mejs.MepDefaults;
+  const globals = Adapt.course.get('_globals')?._components?._media;
+  for (const k in ariaLabelMappings) {
+    dynamicLabels[ariaLabelMappings[k]] = globals[k] ?? ariaLabelMappings[k];
+    fixedDefaults[k] = dynamicLabels[ariaLabelMappings[k]];
+  }
+});
