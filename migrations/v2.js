252b186d53b2dcde775138cae9dcdcd5c7c24a55
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2025-02-18 15:18:07 +0000

    missing v2 migrations added

--- migrations/v2.js
+++ migrations/v2.js
@@ -1,8 +1,92 @@
 import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
 
+describe('Media - v2.0.1 to v2.0.2', async () => {
+  let mediaComponents;
+  whereFromPlugin('Media - from v2.0.1', { name: 'adapt-contrib-media', version: '>= 2.0.0 <2.0.2' });
+  whereContent('Media - where media', async (content) => {
+    mediaComponents = content.filter(({ _component }) => _component === 'media');
+    return mediaComponents.length;
+  });
+  mutateContent('Media - add _startLanguage attribute to media component', async (content) => {
+    mediaComponents.forEach(mediaComponent => {
+      mediaComponent._startLanguage = 'en';
+    });
+    return true;
+  });
+  checkContent('Media - check _startLanguage attribute', async (content) => {
+    const isValid = mediaComponents.every(({ _startLanguage }) => _startLanguage !== undefined);
+    if (!isValid) throw new Error('Media - _startLanguage attribute missing');
+    return true;
+  });
+  updatePlugin('Media - update to v2.0.2', { name: 'adapt-contrib-media', version: '2.0.2', framework: '>=2.0.0' });
+});
+
+describe('Media - v2.0.2 to v2.0.3', async () => {
+  let mediaComponents;
+  whereFromPlugin('Media - from v2.0.2', { name: 'adapt-contrib-media', version: '<2.0.3' });
+  whereContent('Media - where media', async (content) => {
+    mediaComponents = content.filter(({ _component }) => _component === 'media');
+    return mediaComponents.length;
+  });
+  mutateContent('Media - add _setCompletionOnView attribute to media component transcript', async (content) => {
+    mediaComponents.forEach(mediaComponent => {
+      mediaComponent._transcript._setCompletionOnView = false;
+    });
+    return true;
+  });
+  checkContent('Media - check _setCompletionOnView attribute', async (content) => {
+    const isValid = mediaComponents.every(({ _transcript }) => _transcript._setCompletionOnView !== undefined);
+    if (!isValid) throw new Error('Media - _setCompletionOnView attribute missing');
+    return true;
+  });
+  updatePlugin('Media - update to v2.0.3', { name: 'adapt-contrib-media', version: '2.0.3', framework: '>=2.0.0' });
+});
+
+describe('Media - v2.0.3 to v2.0.4', async () => {
+  let mediaComponents;
+  whereFromPlugin('Media - from v2.0.3', { name: 'adapt-contrib-media', version: '<2.0.4' });
+  whereContent('Media - where media', async (content) => {
+    mediaComponents = content.filter(({ _component }) => _component === 'media');
+    return mediaComponents.length;
+  });
+  mutateContent('Media - add _allowFullScreen attribute to media component', async (content) => {
+    mediaComponents.forEach(mediaComponent => {
+      mediaComponent._allowFullScreen = false;
+    });
+    return true;
+  });
+  checkContent('Media - check _allowFullScreen attribute', async (content) => {
+    const isValid = mediaComponents.every(({ _allowFullScreen }) => _allowFullScreen !== undefined);
+    if (!isValid) throw new Error('Media - _allowFullScreen attribute missing');
+    return true;
+  });
+  updatePlugin('Media - update to v2.0.4', { name: 'adapt-contrib-media', version: '2.0.4', framework: '>=2.0.0' });
+});
+
+describe('Media - v2.0.4 to v2.0.5', async () => {
+  let mediaComponents;
+  whereFromPlugin('Media - from v2.0.4', { name: 'adapt-contrib-media', version: '<2.0.5' });
+  whereContent('Media - where media', async (content) => {
+    mediaComponents = content.filter(({ _component }) => _component === 'media');
+    return mediaComponents.length;
+  });
+  mutateContent('Media - add webm attribute to media component', async (content) => {
+    mediaComponents.forEach(mediaComponent => {
+      mediaComponent.webm = '';
+    });
+    return true;
+  });
+  checkContent('Media - check webm attribute', async (content) => {
+    const isValid = mediaComponents.every(({ _media }) => _media.webm !== undefined);
+    if (!isValid) throw new Error('Media - webm attribute missing');
+    return true;
+  });
+  updatePlugin('Media - update to v2.0.5', { name: 'adapt-contrib-media', version: '2.0.5', framework: '>=2.0.0' });
+});
+
 describe('Media - v2.0.5 to v2.0.6', async () => {
   let mediaComponents;
-  whereFromPlugin('Media - from v2.0.6', { name: 'adapt-contrib-media', version: '<2.0.6' });
+  whereFromPlugin('Media - from v2.0.5', { name: 'adapt-contrib-media', version: '<2.0.6' });
   whereContent('Media - where media', async (content) => {
     mediaComponents = content.filter(({ _component }) => _component === 'media');
     return mediaComponents.length;
